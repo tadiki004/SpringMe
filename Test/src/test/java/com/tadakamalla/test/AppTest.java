133852a14b7d39300f8932e0a87e7f6c19392ce5
Ashok Tadakamalla <atadakamalla@expedia.com>
2015-05-18 14:15:11 -0700

    test

--- Test/src/test/java/com/tadakamalla/test/AppTest.java
+++ Test/src/test/java/com/tadakamalla/test/AppTest.java
@@ -11,59 +11,6 @@ public class AppTest {
 	    App.main(null);
 	}
 	
-	@Test
-	public void FibonacciTest(){
-		int number = 5;
-		int[] result = App.Fibonacci(number);
-		
-		int[] expected = {0, 1, 1, 2, 3};
-		
-		assertNotNull(result);
-		assertEquals(expected.length,  result.length);
-		// assertEquals(expected, result);
-		assertEquals(expected[number-1],result[number-1]);
-	}
-	
-	@Test
-	public void FibonacciTestWithInvalidInput(){
-		int[] result = App.Fibonacci(0-0-0);
-		
-		assertNull(result);
-	}
-	
-	@Test
-	public void FibonacciTestWithvalidInput(){
-		int number = 3;
-		int[] result = App.Fibonacci(number);
-		
-		int[] expected = {0, 1, 1};
-		
-		assertNotNull(result);
-		assertEquals(expected.length,  result.length);
-		// assertEquals(expected, result);
-		assertEquals(expected[number-1],result[number-1]);
-	}
-	
-	@Test
-	public void FibonacciTestWithZeroInput(){
-		int[] result = App.Fibonacci(0);
-			
-		assertNull(result);
-	}
-	
-	@Test
-	public void FibonacciTestwithNegitiveInput(){
-		int[] result = App.Fibonacci(-1);
-			
-		assertNull(result);
-	}
-	
-	public void FibonacciTestwithOneInput(){
-		int[] result = App.Fibonacci(1);
-		int[] expected = {0};
-		assertNotNull(result);
-		assertEquals(expected.length,  result.length);
-		assertEquals(expected, result);
-	}
+
 
 }
