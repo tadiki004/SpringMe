418b82faa7952036b144f55ce5921a928c63ef32
Ashok Tadakamalla <atadakamalla@expedia.com>
2015-05-18 14:29:43 -0700

    Added new branch "Dev"

--- Test/src/test/java/com/tadakamalla/test/AppTest.java
+++ Test/src/test/java/com/tadakamalla/test/AppTest.java
@@ -23,5 +23,49 @@ public class AppTest {
 		// assertEquals(expected, result);
 		assertEquals(expected[number-1],result[number-1]);
 	}
+	@Test
+	public void FibonacciTestWithInvalidInput(){
+		int[] result = App.Fibonacci(0-0-0);
+		
+		assertNull(result);
+	}
+	
+	@Test
+	public void FibonacciTestWithvalidInput(){
+		int number = 3;
+		int[] result = App.Fibonacci(number);
+		
+		int[] expected = {0, 1, 1};
+		
+		assertNotNull(result);
+		assertEquals(expected.length,  result.length);
+		// assertEquals(expected, result);
+		assertEquals(expected[number-1],result[number-1]);
+	}
+	
+	@Test
+	public void FibonacciTestWithZeroInput(){
+		int[] result = App.Fibonacci(0);
+			
+		assertNull(result);
+	}
+	
+	@Test
+	public void FibonacciTestwithNegitiveInput(){
+		int[] result = App.Fibonacci(-1);
+			
+		assertNull(result);
+	}
+	
+	public void FibonacciTestwithOneInput(){
+		int[] result = App.Fibonacci(1);
+		int[] expected = {0};
+		assertNotNull(result);
+		assertEquals(expected.length,  result.length);
+		assertEquals(expected, result);
+	}
+
+	
+	
 
 }
